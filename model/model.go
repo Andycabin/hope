554d8227f9f310df8670b32da7ca8fa748ac6409
Andyshy <2217556994@qq.com>
2019-05-13 22:29:42 +0800

    Add files via upload

--- model/model.go
+++ model/model.go
@@ -1,1 +1,15 @@
+// model
+package model
 
+type Profile struct {
+	Name       string `name:"name"`
+	Gender     string `name:"gender"`
+	Age        int    `name:"age"`
+	Height     int    `name:"height"`
+	Weight     int    `name:"weight"`
+	Income     string `name:"income"`
+	Marriage   string `name:"marriage"`
+	Education  string `name:"education"`
+	Occupation string `name:"occupation"`
+	Hukou      string `name:"hukou"`
+}
